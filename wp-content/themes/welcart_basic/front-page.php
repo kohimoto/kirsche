8c207a3312b024904958bcec767315bada38849f
fujimoto <kohimotoj@gmail.com>
2017-06-09 00:17:56 +0900

    css sp & top

--- wp-content/themes/welcart_basic/front-page.php
+++ wp-content/themes/welcart_basic/front-page.php
@@ -1,22 +1,22 @@
 <?php get_header(); ?>
-	<h2>About this shop</h2>	
+	<h2>Thought for kirsche</h2>	
 	<div class="button"><a href="/about"><button>About</button></a></div>
-	<h2>Take a look!</h2>	
+	<h2>Take a look!</h2>
 
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
-		
+
 		<?
 		//2017.05.22 kohinata
-		query_posts('posts_per_page=-1&cat=2&post_status=publish'); 
+		query_posts('posts_per_page=-1&cat=2&post_status=publish');
 		?>
 		<?php if ( 'page' == get_option('show_on_front') ): ?>
 			<div class="sof">
 				<?php if (have_posts()) : the_post(); ?>
 					<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-	
+
 						<h2 class="entry-title"><?php the_title(); ?></h2>
-				
+
 						<div class="entry-content">
 							<?php the_content(); ?>
 						</div>
@@ -29,10 +29,10 @@
 		<?php else: ?>
 
 			<section class="front-il cf">
-			
+
 				<?php if( have_posts() ) : ?>
 					<?php while( have_posts() ) : the_post(); usces_the_item(); ?>
-					
+
 						<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 							<div class="itemimg">
 								<a href="<?php the_permalink(); ?>"><?php usces_the_itemImage( 0, 300, 300 ); ?></a>
@@ -43,21 +43,21 @@
 							<?php endif; ?>
 							<div class="itemname red"><a href="<?php the_permalink(); ?>"  rel="bookmark" class="red"><?php usces_the_itemName(); ?></a></div>
 							<div class="itemprice red"><?php usces_crform( usces_the_firstPrice('return'), false, false ); ?> JPY</div>
-						
+
 						</article>
-					
+
 					<?php endwhile; ?>
-					
+
 				<?php else: ?>
-					
+
 					<p class="no-date"><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-				
+
 				<?php endif; ?>
-			
+
 			</section><!-- .front-il -->
 
 		<?php endif; ?>
-				
+
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
