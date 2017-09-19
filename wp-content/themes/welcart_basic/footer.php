5a5d025568e88a1b5662de254bac409218004850
kohinata <kohimotoj@gmail.com>
2017-09-19 15:30:51 +0900

    本番に合わせる。

--- wp-content/themes/welcart_basic/footer.php
+++ wp-content/themes/welcart_basic/footer.php
@@ -1,34 +1,33 @@
-</div><!-- #main -->
+	</div><!-- #main -->
+
 	<?php if(  is_home() || is_front_page() ): ?>
-<<<<<<< HEAD
-<div class="info_area">
-<h2>Thanks for comming</h2>
-<h3>Hope to see you soon. Thank you &#9829;<br>
-=======
 		<div class="wave">
 			<img src="/wp-content/themes/welcart_basic/images/about/wave_b_g.gif" alt="">
 		</div>
   	<div class="info_area">
   	  <h2>Thanks for comming</h2>
   	  <h3>Hope to see you soon. Thank you &#9829;<br>
->>>>>>> 3902859f47e8fb67e8a88296a8cdbc431bcc35e4
 This shop is all created, managed, supervised by <a target=_blank href="https://kohimoto.com">kohimoto</a>.</h3>
-</div>
+  	</div>
+
 	<?php endif; ?>
+
 	<?php if(! wp_is_mobile()): ?>
 
-<div id="toTop" class="wrap fixed"><a href="#masthead"><i class="fa fa-chevron-circle-up"></i></a></div>
+		<div id="toTop" class="wrap fixed"><a href="#masthead"><i class="fa fa-chevron-circle-up"></i></a></div>
 
 	<?php endif; ?>
 
-<footer id="colophon" role="contentinfo">
-<p class="copyright">Copyright &copy; 2017 kohimoto. All Rights Reserved. Designed by <a href="http://kohimoto.com/" target="_blank">kohimoto</a>.</p>
+	<footer id="colophon" role="contentinfo">
 
-</footer><!-- #colophon -->
-<script src="/wp-content/themes/welcart_basic/js/jquery.min.js"></script>
-<script src="/wp-content/themes/welcart_basic/js/navnemu.js"></script>
-<script src="/wp-content/themes/welcart_basic/js/animation.js"></script><!-- navmenu -->
+
+		<p class="copyright">Copyright &copy; 2017 kohimoto. All Rights Reserved. Designed by <a href="http://kohimoto.com/" target="_blank">kohimoto</a>.</p>
+
+	</footer><!-- #colophon -->
+	<script src="/wp-content/themes/welcart_basic/js/jquery.min.js"></script>
+	<script src="/wp-content/themes/welcart_basic/js/navnemu.js"></script>
+	<script src="/wp-content/themes/welcart_basic/js/animation.js"></script><!-- navmenu -->
 
 	<?php wp_footer(); ?>
-</body>
+	</body>
 </html>
